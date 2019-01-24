225f4bd315bdf1213b6ce181a2dd76ea9717146e
sevenbamboos <sevenbamboos@gmail.com>
2019-01-24 22:03:06 +1300

    What: test for core
    Why:
    
    How:

--- test/common.spec.js
+++ test/common.spec.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const assert = require('assert');
-const { Tuple2, Facing, parseFacing2Str, parseStr2Facing }  = require('../lib/common');
+const { Tuple2, Facing, parseFacing2Str, parseStr2Facing, turnLeft, turnRight, moveForward }  = require('../lib/common');
 
 describe('test Tuple2', () => {
 
@@ -34,6 +34,23 @@ describe('test Facing', () => {
     assert.throws(
       () => parseStr2Facing('not-a-facing')
     );
+  });
+
+  it('can turn around and move', () => {
+    assert.equal(Facing.WEST, turnLeft(Facing.NORTH));
+    assert.equal(Facing.SOUTH, turnLeft(Facing.WEST));
+    assert.equal(Facing.EAST, turnLeft(Facing.SOUTH));
+    assert.equal(Facing.NORTH, turnLeft(Facing.EAST));
+
+    assert.equal(Facing.EAST, turnRight(Facing.NORTH));
+    assert.equal(Facing.SOUTH, turnRight(Facing.EAST));
+    assert.equal(Facing.WEST, turnRight(Facing.SOUTH));
+    assert.equal(Facing.NORTH, turnRight(Facing.WEST));
+
+    assert(new Tuple2(3, 2).isSame(moveForward(Facing.EAST, new Tuple2(2, 2))));
+    assert(new Tuple2(2, 1).isSame(moveForward(Facing.SOUTH, new Tuple2(2, 2))));
+    assert(new Tuple2(1, 2).isSame(moveForward(Facing.WEST, new Tuple2(2, 2))));
+    assert(new Tuple2(2, 3).isSame(moveForward(Facing.NORTH, new Tuple2(2, 2))));
   });
   
 });
---
+++ test/core.spec.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const assert = require('assert');
+const { Tuple2, Facing, parseFacing2Str, parseStr2Facing, turnLeft, turnRight, moveForward }  = require('../lib/common');
+const { Bus, CarPark } = require('../lib/core');
+
+describe('test Bus', () => {
+
+  it('can output', () => {
+    const b1 = new Bus(1);
+    b1.place(new Tuple2(2, 2), Facing.EAST);
+    assert.equal('2, 2, EAST', b1.report());
+  });
+
+  it('can give the next position', () => {
+    const b1 = new Bus(1);
+    b1.place(new Tuple2(2, 2), Facing.EAST);
+    assert(new Tuple2(3, 2).isSame(b1.nextPosition()));
+    // the bus remains the same place after call nextPosition
+    assert.equal('2, 2, EAST', b1.report());
+  });
+
+  it('can turn around and move forward', () => {
+    const b1 = new Bus(1);
+
+    assert.throws(
+      () => b1.turnLeft()
+    );
+
+    assert.throws(
+      () => b1.turnRight()
+    );
+
+    assert.throws(
+      () => b1.moveForward()
+    );
+
+    b1.place(new Tuple2(2, 2), Facing.EAST);
+    b1.turnLeft();
+    assert.equal('2, 2, NORTH', b1.report());
+
+    b1.turnRight();
+    assert.equal('2, 2, EAST', b1.report());
+
+    b1.moveForward();
+    assert.equal('3, 2, EAST', b1.report());
+
+  });
+  
+});
+
+describe('test CarPark', () => {
+
+  it('can check valid position', () => {
+    const b1 = new Bus(1);
+    b1.place(new Tuple2(2, 2), Facing.EAST);
+    const carPark = new CarPark(5, 5);
+    assert(carPark.isFree(2, 2));
+
+    carPark.addBus(b1);
+    assert(!carPark.isFree(new Tuple2(2, 2)));
+    
+    assert(carPark.isInRange(new Tuple2(2, 2)));
+    assert(carPark.isInRange(new Tuple2(0, 0)));
+    assert(carPark.isInRange(new Tuple2(4, 4)));
+
+    assert(!carPark.isInRange(new Tuple2(4, 5)));
+    assert(!carPark.isInRange(new Tuple2(5, 4)));
+    assert(!carPark.isInRange(new Tuple2(5, 5)));
+    assert(!carPark.isInRange(new Tuple2(0, -1)));
+    assert(!carPark.isInRange(new Tuple2(-1, 0)));
+    assert(!carPark.isInRange(new Tuple2(-1, -1)));
+  });
+  
+});
